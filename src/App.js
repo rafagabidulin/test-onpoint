c7e15465f6890c5e0f927bf99fd1313e84a62e0f
rafagabidulin <rafagabidulin@gmail.com>
2022-06-07 23:59:36 +0300

    created Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Slide1 from './components/Container/Slide1/Slide1';
+import Slide2 from './components/Container/Slide2/Slide2';
+import Slide3 from './components/Container/Slide3/Slide3';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div />
+    <div>
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Slide1 />} />
+          <Route path="/slide2" element={<Slide2 />} />
+          <Route path="/slide3" element={<Slide3 />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
